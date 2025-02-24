ba98f0b390b40ff92aa6c14df19eb7017bc52c9f
Edson Copque <ecop@disroot.org>
2025-02-24 16:47:01 -0300

    Creating...

--- server.py
+++ server.py
@@ -1,2 +1,10 @@
 # File: /group_chat/server.py
 
+import socket
+import threading
+
+HOST = 'localhost'
+PORT = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
