22e6f1d01a21e9cd4682219a629587e8553af36d
Edson Copque <ecop@disroot.org>
2025-02-24 16:49:04 -0300

    Update.

--- server.py
+++ server.py
@@ -8,3 +8,8 @@ PORT = 55555
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
+server.listen()
+
+while True:
+    client, addr = server.accept()
+    print(client)
