769f92907a4827c1ddf8bc4efa1d28c76a259aa8
Edson Copque <ecop@disroot.org>
2025-02-25 15:24:42 -0300

    Update.

--- client.py
+++ client.py
@@ -6,39 +6,53 @@ from tkinter import * #1:
 import tkinter #1:
 from tkinter import simpledialog #1:
 
-class Chat:
-    def __init__(self):
-        HOST = 'localhost'
-        PORT = 55556
+class Chat: #2:
+    def __init__(self): #2:
+        HOST = 'localhost' #2:
+        PORT = 55556 #2:
 
-        # self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # self.client.connect((HOST, PORT))
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #2:
+        self.client.connect((HOST, PORT)) #2:
 
-        login = Tk() # tkinter * # Instance/GUI graphical interface.
-        login.withdraw() # tkinter * # Hide the main window.
+        login = Tk() #3: # tkinter * # Instance/GUI graphical interface.
+        login.withdraw() #3: # tkinter * # Hide the main window.
 
-        self.loaded_window = False # Has not yet been loaded or initialized.
-        self.active = True # Ready to send and receive messages.
+        self.loaded_window = False #3: # Has not yet been loaded or initialized.
+        self.active = True #3: # Ready to send and receive messages.
 
-        self.name = simpledialog.askstring('Name', 'Enter your name: ', parent=login) # dialog box will appear over the login window.
-        self.room = simpledialog.askstring('Room', 'Enter the room: ', parent=login)
-        self.window()
+        self.name = simpledialog.askstring('Name', 'Enter your name: ', parent=login) #3: # dialog box will appear over the login window.
+        self.room = simpledialog.askstring('Room', 'Enter the room: ', parent=login) #3:
+        self.window() #4:
 
-    def window(self):
-        self.root = Tk()
-        self.root.geometry('800x800')
-        self.root.title('Group Chat')
+    def window(self): #4:
+        self.root = Tk() #4:
+        self.root.geometry('800x800') #4:
+        self.root.title('Group Chat') #4:
 
-        self.text_box = Text(self.root)
-        self.text_box.place(relx=0.05, rely=0.01, width=700, height=600)
+        self.text_box = Text(self.root) #4:
+        self.text_box.place(relx=0.05, rely=0.01, width=700, height=600) #4:
 
-        self.send_message = Entry(self.root)
-        self.send_message.place(relx=0.05, rely=0.8, width=500, height=20)
+        self.send_message = Entry(self.root) #4:
+        self.send_message.place(relx=0.05, rely=0.8, width=500, height=20) #4:
 
-        self.button_send = Button(self.root, text='Send', command=self.Send_Message)
-        self.button_send.place(relx=0.7, rely=0.8, width=100, height=20)
+        self.button_send = Button(self.root, text='Send', command=self.Send_Message) #5:
+        self.button_send.place(relx=0.7, rely=0.8, width=100, height=20) #5:
+        self.root.protocol('WM_DELETE_WINDOW', self.close) #5:
 
-        self.root.mainloop() # Make window say open.
+        self.root.mainloop() #4: # Make window say open.
+
+    def close(self): #6:
+        self.root.destroy() #6:
+        self.client.close() #6:
+
+    def connect(self): #7:
+        while True: #7:
+            received = self.client.recv(1024) #7:
+            if received == b'ROOM': #7:
+                self.client.send(self.room.encode()) #7:
+                self.client.send(self.name.encode()) #7:
+            else:
+                self.text_box.insert('end', received.decode()) #7:
 
     def Send_Message(self):
         ...
