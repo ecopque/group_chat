547ac49760ebaa29f00e9798a414fbf58db8e62a
Edson Copque <ecop@disroot.org>
2025-02-25 11:08:02 -0300

    Update

--- client.py
+++ client.py
@@ -6,3 +6,10 @@ from tkinter import * #1:
 import tkinter #1:
 from tkinter import simpledialog #1:
 
+class Chat:
+    def __init__(self):
+        HOST = 'localhost'
+        PORT = 55556
+
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(HOST, PORT)
