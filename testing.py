1acc6217ad6920be95ffe8629ae616adb3fbff1f
Edson Copque <ecop@disroot.org>
2025-02-26 15:52:56 -0300

    Update.

--- testing.py
+++ testing.py
@@ -5,3 +5,9 @@ def broadcast(my_room, my_messagem):
         if isinstance(my_messagem, str):
             my_messagem = my_messagem.encode()
         i1.send(my_messagem)
+
+def send_message(name, room, client):
+    while True:
+        listen_message = client.recv(1024)
+        my_message = f'{name} {listen_message.decode()}\n'
+        broadcast(room, my_message)
